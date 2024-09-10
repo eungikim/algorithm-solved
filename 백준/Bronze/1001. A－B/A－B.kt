3626967bb4cf882efb433ea5b435469d83fa9f39
Eungi Kim <eungikim.me@gmail.com>
2024-09-10 17:43:11 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 13240 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val numFirst = scanner.nextInt()
+    val numSecond = scanner.nextInt()
+    println(numFirst - numSecond)
+}
